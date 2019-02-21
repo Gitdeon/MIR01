b0eefd991e97324b146138af366196b74234caba
Gitdeon <Gitdeon@github.com>
2019-02-21 12:05:00 +0100

    .

--- websearch.new/htmlprocess2.c
+++ websearch.new/htmlprocess2.c
@@ -55,67 +55,31 @@ int main(int argc, char *argv[]) {
 }
 */
 
-// Define our struct for accepting LCs output
-struct BufferStruct
-{
-   char * buffer;
-   size_t size;
-};
-
-// This is the function we pass to LC, which writes the output to a BufferStruct
-static size_t WriteMemoryCallback
-(void *ptr, size_t size, size_t nmemb, void *data)
-{
-   size_t realsize = size * nmemb;
+void GetWebPage(char * myurl) {
+   CURL *curl;
+   FILE *fp;
+   int result;
    
-   struct BufferStruct * mem = (struct BufferStruct *) data;
+   fp = fopen(argv[2], 'wb');
    
-   mem->buffer = realloc(mem->buffer, mem->size + realsize + 1);
+   curl = curl_easy_init();
    
-   if ( mem->buffer )
-   {
-      memcpy( &( mem->buffer[ mem->size ] ), ptr, realsize );
-      mem->size += realsize;
-      mem->buffer[ mem->size ] = 0;
-   }
-   return realsize;
+   curl_easy_setopt(curl, CURLOPT_URL, myurl);
+   curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp)
+   curl_easy_setopt(curl, CURLOPT_FAILONERROR, 1L);
+   
+   result = curl_easy_perform(curl);
+   if (result == CURLE_OK)
+      printf("Download succesful!\n");
+   else
+      printf("ERROR: %s\n", curl_easy_strerror(result));
+   fpclose(fp);
+   
+   curl_easy_cleanup(curl);
 }
 
-
-int main()
-{
-   
-   curl_global_init( CURL_GLOBAL_ALL );
-   CURL * myHandle;
-   CURLcode result; // We’ll store the result of CURL’s webpage retrieval, for simple error checking.
-   struct BufferStruct output; // Create an instance of out BufferStruct to accept LCs output
-   output.buffer = NULL;
-   output.size = 0;
-   myHandle = curl_easy_init ( ) ;
-   
-   /* Notice the lack of major error checking, for brevity */
-   
-   curl_easy_setopt(myHandle, CURLOPT_WRITEFUNCTION, WriteMemoryCallback); // Passing the function pointer to LC
-   curl_easy_setopt(myHandle, CURLOPT_WRITEDATA, (void *)&output); // Passing our BufferStruct to LC
-   curl_easy_setopt(myHandle, CURLOPT_URL, "http://www.liacs.nl");
-   result = curl_easy_perform( myHandle );
-   curl_easy_cleanup( myHandle );
-   
-   FILE * fp;
-   fp = fopen( "/tmp/example.html","w");
-   if( !fp )
-      return 1;
-   fprintf(fp, output.buffer );
-   fclose( fp );
-   
-   if( output.buffer )
-   {
-      free ( output.buffer );
-      output.buffer = 0;
-      output.size = 0;
-   }
-   
-   printf("LibCurl rules!\n");
+int main(int argc, char ** argv) {
+   if( argc != 2 ) return 1;
+   GetWebPage(argv[1]);
    return 0;
 }
-
