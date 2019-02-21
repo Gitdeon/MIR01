38ee964304235161316d4dfdf8360c4f80e98daa
Gitdeon <Gitdeon@github.com>
2019-02-21 12:33:35 +0100

    m

--- websearch.new/htmlprocess2.c
+++ websearch.new/htmlprocess2.c
@@ -6,80 +6,59 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
 #include <string.h>
 #include <curl/curl.h>
-#include <haut/string_util.h>
-#include <haut/haut.h>
-#include <haut/tag.h>
 
-/*
- typedef struct {
-   FILE *tmp1; // a pointer to the file save the data of HTML file
-   FILE *tmp2; // a pointer to the file save web-links from the URL
-   // .. Add your own datastructures here ...
-} state_t;
+struct string {
+   char *ptr;
+   size_t len;
+};
 
-string data;
-
-size_t write_callback (void *buffer, size_t size, size_t nmemb, FILE *stream) {
-   for (int c = 0; c<size*nmemb; c++) {
-      data.push_back(buffer[c]);
+void init_string(struct string *s) {
+   s->len = 0;
+   s->ptr = malloc(s->len+1);
+   if (s->ptr == NULL) {
+      fprintf(stderr, "malloc() failed\n");
+      exit(EXIT_FAILURE);
    }
+   s->ptr[0] = '\0';
+}
+
+size_t writefunc(void *ptr, size_t size, size_t nmemb, struct string *s)
+{
+   size_t new_len = s->len + size*nmemb;
+   s->ptr = realloc(s->ptr, new_len+1);
+   if (s->ptr == NULL) {
+      fprintf(stderr, "realloc() failed\n");
+      exit(EXIT_FAILURE);
+   }
+   memcpy(s->ptr+s->len, ptr, size*nmemb);
+   s->ptr[new_len] = '\0';
+   s->len = new_len;
+   
    return size*nmemb;
 }
 
-void GetWebPage(char * myurl) {
-   CURL * curl;
-   curl_global_init(CURL_GLOBAL_ALL);
-   curl = curl_easy_init();
-   curl_easy_setopt(curl, CURLOPT_URL, myurl);
-   curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &writeCallback);
-   curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
-   
-   curl_easy_perform(curl);
-   
-   cout << endl << data << endl;
-   cin.get();
-   
-   curl_easy_cleanup(curl);
-   curl_global_cleanup();
-}
-
-int main(int argc, char *argv[]) {
-   if (argc != 2) return 1;
-   GetWebPage( argv[1]);
-   
-   
-   return 0;
-}
-*/
-
-void GetWebPage(char * myurl) {
+int main(void)
+{
    CURL *curl;
-   FILE *fp;
-   int result;
-   
-   fp = fopen(argv[2], 'wb');
+   CURLcode res;
    
    curl = curl_easy_init();
-   
-   curl_easy_setopt(curl, CURLOPT_URL, myurl);
-   curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp)
-   curl_easy_setopt(curl, CURLOPT_FAILONERROR, 1L);
-   
-   result = curl_easy_perform(curl);
-   if (result == CURLE_OK)
-      printf("Download succesful!\n");
-   else
-      printf("ERROR: %s\n", curl_easy_strerror(result));
-   fpclose(fp);
-   
-   curl_easy_cleanup(curl);
-}
-
-int main(int argc, char ** argv) {
-   if( argc != 2 ) return 1;
-   GetWebPage(argv[1]);
+   if(curl) {
+      struct string s;
+      init_string(&s);
+      
+      curl_easy_setopt(curl, CURLOPT_URL, "curl.haxx.se");
+      curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writefunc);
+      curl_easy_setopt(curl, CURLOPT_WRITEDATA, &s);
+      res = curl_easy_perform(curl);
+      
+      printf("%s\n", s.ptr);
+      free(s.ptr);
+      
+      /* always cleanup */
+      curl_easy_cleanup(curl);
+   }
    return 0;
 }
